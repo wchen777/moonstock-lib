69446262a561601c3a12a4c18683a39433516f52
Will Chen <william_b_chen@brown.edu>
2021-12-22 17:15:16 -0500

    setup package

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # moonstock-lib
-Python library and API for financial modeling and calculations. 
+Python library and API for financial modeling and calculations.
 
 Part of Moonstock-API.
+
+
+## Building the library
+
+`python setup.py bdist_wheel`
+
+`pip install /path/to/wheelfile.whl`
+
+Once installed, import library using
+
+`import moonstock`
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='moonstock',
+    packages=find_packages(),
+    version='0.1.0',
+    description='API for financial modeling and calculations.',
+    author='Will Chen, Ankith Hiremath',
+    license='MIT',
+    install_requires=['numpy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
